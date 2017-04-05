35579a831823fb98a49952df5ebb9b5a2d47bdb5
Daniel Boulos <cr7boulos@gmail.com>
2017-04-04 21:03:17 -0500

    Setting up the velocity renderer

---
+++ src/main/java/longbottom/util/ViewUtil.java
@@ -0,0 +1,32 @@
+package longbottom.util;
+
+// This file was copied from the following link:
+// https://github.com/tipsy/spark-basic-structure/blob/master/src/main/java/app/util/ViewUtil.java
+
+import org.apache.velocity.app.*;
+import org.eclipse.jetty.http.*;
+import spark.*;
+import spark.template.velocity.*;
+import java.util.*;
+
+
+public class ViewUtil {
+
+
+    public static String render(Request request, Map<String, Object> model, String templatePath) {
+
+        //will uncomment these lines if needed in application D.B
+
+        //model.put("currentUser", getSessionCurrentUser(request));
+        //model.put("WebPath", Path.Web.class); // Access application URLs from templates
+        return strictVelocityEngine().render(new ModelAndView(model, templatePath));
+    }
+
+    private static VelocityTemplateEngine strictVelocityEngine() {
+        VelocityEngine configuredEngine = new VelocityEngine();
+        configuredEngine.setProperty("runtime.references.strict", true);
+        configuredEngine.setProperty("resource.loader", "class");
+        configuredEngine.setProperty("class.resource.loader.class", "org.apache.velocity.runtime.resource.loader.ClasspathResourceLoader");
+        return new VelocityTemplateEngine(configuredEngine);
+    }
+}
