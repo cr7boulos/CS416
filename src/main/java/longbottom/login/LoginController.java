33e245781c3c74ee55c6510b6fd25637b777b9a3
Kyle Brantley <KylePhoneDial@gmail.com>
2017-04-07 19:24:03 -0500

    Placed loginController into proper folder

---
+++ src/main/java/longbottom/login/LoginController.java
@@ -0,0 +1,10 @@
+package longbottom.login;
+
+import spark.*;
+import longbottom.DAO.*;
+import longbottom.util.ViewUtil;
+
+public class LoginController {
+    //public static Route isAuthenticated = (Request request, Response response) -> {
+    //}
+}
