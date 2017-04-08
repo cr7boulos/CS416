85c7b1a06279bc196197e7c736b24907a1689858
Japji Multani <jpjsingh16@gmail.com>
2017-04-08 07:52:33 -0500

    updateProjects method which returns a string based on if a project was successfully added to the database

--- src/main/java/longbottom/projects/ProjectController.java
+++ src/main/java/longbottom/projects/ProjectController.java
@@ -7,15 +7,20 @@ import spark.Response;
 import spark.Route;
 import java.util.HashMap;
 import java.util.Map;
-/**
- * Created by Japji on 4/7/2017.
- */
+
 public class ProjectController {
 
     /*/
     public static Route updateProjects = (Request request, Response response) -> {
         LoginController.isAuthenticated(request, response);
-
+        if( DAO.updateProjects(Integer.parseInt(request.queryParams("projectId")), request.queryParams("name"), request.queryParams("description"), Long.parseLong( request.queryParams("time"), Integer.parseInt( request.queryParams("manager")) == false)
+        {
+            return "Update Unsuccessful";
+        }
+        else
+        {
+            return "Update Successful";
+        }
 
     }
     /*/
