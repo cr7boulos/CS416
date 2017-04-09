439528766cbc476a49bbd39bf0b35aeb8188020e
Daniel Boulos <cr7boulos@gmail.com>
2017-04-09 17:50:54 -0500

    Created project velocity template

--- src/main/java/longbottom/accounts/Dashboard.java
+++ src/main/java/longbottom/accounts/Dashboard.java
@@ -8,10 +8,7 @@ import spark.Response;
 import spark.Route;
 
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 /**
  * Created by cr7bo on 4/7/2017.
@@ -21,16 +18,29 @@ public class Dashboard {
     public static Route userDashboard = (Request request, Response response) -> {
         //int userId = LoginController.isAuthenticated(request, response);
 
+        //this is test code; will be deleted in final
+        //implementation
         Map<String, Object> model1 = new HashMap<>();
-        model1.put("name", "Daniel");
+        model1.put("name", "Random name");
+        model1.put("description", "Random project");
+        model1.put("manager", "Zhang");
+        model1.put("startDate", "2012-3-12");
+        model1.put("in", 0);
         Map<String, Object> model2 = new HashMap<>();
+        model2.put("name", "Steering Elections");
         model2.put("description", "Emergence of consensus");
+        model2.put("manager", "Zhang");
+        model2.put("startDate", "2012-3-12");
+        model2.put("in", 1);
+
 
         List<Map<String, Object>> list = new ArrayList<>();
         list.add(model1);
         list.add(model2);
+
         Map<String, Object> model3 = new HashMap<>();
         model3.put("dataList", list);
+
         return ViewUtil.render(model3, "/velocity/dashboard.vm");
         /*if(userId == 0){
             // DAO.getProjectRequests()
@@ -42,7 +52,7 @@ public class Dashboard {
             //List<Map<String,Object>> projectDetails = DAO.getAllProjects();
             model.put("projects", projectDetails);
         }
-*/
+        */
     };
 
     public static Route projectPartial = (Request request, Response response) -> {
