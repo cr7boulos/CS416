c3531ee06ebda8750d8faff87eaa98c633676a75
Jacob <jacoford21@gmail.com>
2017-04-16 14:27:33 -0500

    Made the updateProject and newProject methods compatible with the current DAO

--- src/main/java/longbottom/projects/ProjectController.java
+++ src/main/java/longbottom/projects/ProjectController.java
@@ -3,39 +3,52 @@ package longbottom.projects;
 import longbottom.DAO.DAO;
 import longbottom.login.LoginController;
 import spark.Response;
-import longbottom.util.ViewUtil;
 import spark.Request;
-import spark.Response;
 import spark.Route;
-import java.util.HashMap;
-import java.util.Map;
 
 public class ProjectController {
 
-    /*public static Route newProjects = (Request request, Response response) -> {
+    public static Route newProject = (Request request, Response response) -> {
         LoginController.isAuthenticated(request, response);
-        if (DAO.createProject(
-                request.queryParams("name"),
-                request.queryParams("description"),
-                request.queryParams("time"),
-                Integer.parseInt( request.queryParams("manager"))) == false)
-        return "Update Unsuccessful";
-        else
-        return "Update Successful";
-    };*/
 
-    /*/
-    public static Route updateProjects = (Request request, Response response) -> {
-        LoginController.isAuthenticated(request, response);
-        if (DAO.updateProject(
-                (Integer.parseInt(request.queryParams("projectId"))),
-                request.queryParams("name"),
-                request.queryParams("description"),
-                request.queryParams("time"),
-                Integer.parseInt( request.queryParams("manager"))) == false)
-            return "Update Unsuccessful";
-        else
+        String name = request.queryParams("name");
+        String description = request.queryParams("description");
+        int manager = Integer.parseInt(request.queryParams("manager"));
+
+        if (DAO.createProject(name, description, manager)) {
             return "Update Successful";
+        } else {
+            return "Update Failed";
+        }
     };
-    /*/
+
+
+    public static Route updateProject = (Request request, Response response) -> {
+        LoginController.isAuthenticated(request, response);
+
+        int projectId = Integer.parseInt(request.queryParams("projectId"));
+        String name = request.queryParams("name");
+        String description = request.queryParams("description");
+        String timeStarted = request.queryParams("time");
+        int manager;
+
+        if (request.queryParams("manager") != null) {
+            manager = Integer.parseInt(request.queryParams("manager"));
+            DAO.updateProjectManager(projectId, manager);
+        }
+
+        if (name != null) {
+            DAO.updateProjectName(projectId, name);
+        }
+
+        if (description != null) {
+            DAO.updateProjectDescription(projectId, description);
+        }
+
+        if (timeStarted != null) {
+            DAO.updateProjectTime(projectId, timeStarted);
+        }
+
+        return "Update complete";
+    };
 }
