3daa3d772c839b97a230043a1c455ad5f7d73f9e
Kyle Brantley <KylePhoneDial@gmail.com>
2017-04-20 11:54:10 -0500

    Fixed broadcastMessage()

--- src/main/java/longbottom/chat/Chat.java
+++ src/main/java/longbottom/chat/Chat.java
@@ -1,31 +1,32 @@
 package longbottom.chat;
 
 import org.eclipse.jetty.websocket.api.*;
+import org.eclipse.jetty.websocket.api.Session;
 import org.json.*;
 import java.text.*;
 import java.util.*;
 import java.util.concurrent.ConcurrentHashMap;
 import longbottom.DAO.*;
+import spark.*;
+
 import static j2html.TagCreator.*;
 import static spark.Spark.*;
 public class Chat {
 
-    static Map<Session, String> userUsernameMap = new ConcurrentHashMap<>();
+    // Map<projectId, all users in the project chat room>
+    static Map<Integer, Map<Session, String>> chatMap = new ConcurrentHashMap<>();
     static int nextUserNumber = 1;
 
-    /*
+    public static void broadcastMessage(int userId, String message, int projectId) {
+        //add chat message to the database
+        DAO.createChatMessage(userId, message);
 
-    public static void main(String[] args){
-    }
-
-    */
-
-    public static void broadcastMessage(String sender, String message) {
-        userUsernameMap.keySet().stream().filter(Session::isOpen).forEach(session -> {
+        // Find the chat room and send a message to all users in that chat room
+        chatMap.get(projectId).keySet().stream().filter(Session::isOpen).forEach(session -> {
             try {
                 session.getRemote().sendString(String.valueOf(new JSONObject()
-                        .put("userMessage", createHtmlMessageFromSender(sender, message))
-                        .put("userlist", userUsernameMap.values())
+                        .put("userMessage", createHtmlMessageFromSender(userId, message))
+                        .put("userlist", chatMap.get(projectId).values())
                 ));
             } catch (Exception e) {
                 e.printStackTrace();
@@ -34,8 +35,7 @@ public class Chat {
     }
 
     //Creates HTML card for the message
-    private static String createHtmlMessageFromSender(String sender, String message) {
-        //DAO.createChatMessage(sender, message, time?);
+    private static String createHtmlMessageFromSender(int sender, String message) {
         return article().with(
                 b(sender + " says:"),
                 p(message),
