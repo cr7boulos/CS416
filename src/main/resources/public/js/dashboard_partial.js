4c8a7eb5421626106e8179f33d57e10d860a4fe8
Daniel Boulos <cr7boulos@gmail.com>
2017-04-23 16:29:34 -0500

    Working: sending email

--- src/main/resources/public/js/dashboard_partial.js
+++ src/main/resources/public/js/dashboard_partial.js
@@ -102,6 +102,7 @@ var email = function () {
     $('#emailButton').click(function () {
         getEmail();
 
+
     });
 
 var getEmail = function () {
@@ -109,6 +110,8 @@ var getEmail = function () {
         $('#dynView').attr('data-userId'),
         function (data) {
             $('#dynView').html(data);
+
+            sendEmail();
 
             //set up the emails for deleting
             $('.deleteEmail').click(function () {
@@ -170,7 +173,7 @@ $(document).ready(function(){
 
     users();
 
-    sendEmail();
+    //sendEmail();
 
     //admin view: show project requests
     if($('#dynView').attr('data-viewType') == 0){
